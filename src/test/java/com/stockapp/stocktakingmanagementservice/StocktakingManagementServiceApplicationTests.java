a53e98c96374e115afb306b6d5a1124b643ffb4a
lmaestredev <luisagustomaestre@gmail.com>
2023-12-18 10:12:35 -0400

    feat: se agregan carpetas de inicio del proyecto

---
+++ src/test/java/com/stockapp/stocktakingmanagementservice/StocktakingManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockapp.stocktakingmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocktakingManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
